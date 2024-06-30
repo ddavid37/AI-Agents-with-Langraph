f8ca7cf4e278201fc67ad331b98996ec77ff186a
Daniel David <ddavid37@montgomerycollege.edu>
2024-06-30 17:19:39 +0300

    first commit

---
+++ StartingFromScratch.py
@@ -0,0 +1,15 @@
+import openai
+import re
+import httpx
+import os
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+from openai import OpenAI
+
+client = OpenAI()
+chat_completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello world"}]
+)
+chat_completion.choices[0].message.content
